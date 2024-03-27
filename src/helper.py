51bbaf9f892e4a56adde82677998ba2e9a38aacd
guneetkft123 <guneet.s@knowledgeflex.com>
2024-03-27 18:03:44 +0530

    New commit

--- src/helper.py
+++ src/helper.py
@@ -5,3 +5,21 @@ Please ensure that your responses are socially unbiased and positive in nature.
 If a question does not make any sense, or is not factually coherent, explain why instead of 
 answering something not correct. If you don't know the answer to a question,
 please don't share false information."""
+
+
+
+#CUSTOM_SYSTEM_PROMPT="You are an advanced assistant that provides translation from English to Hindi"
+CUSTOM_SYSTEM_PROMPT="You are an advanced assistant that provides summarization given any book name"
+
+
+
+
+template="""Use the following pieces of information to answer the user's question.
+If you dont know the answer just say you know, don't try to make up an answer.
+
+Context:{context}
+Question:{question}
+
+Only return the helpful answer below and nothing else
+Helpful answer
+"""
